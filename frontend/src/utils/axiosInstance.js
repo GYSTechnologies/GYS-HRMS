b9105360f4a7b15d8f8d8ae411d3e0db7b563893
Yogesh <yogeshgadhewal28@gmail.com>
2025-09-23 20:23:56 +0530

    resolve cors-9

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   // baseURL: "http://localhost:4000/api",
-    baseURL: "https://hrms-backend-jet.vercel.app/api",
+    baseURL: "https://gys-hrms.vercel.app/api",
   headers: {
     "Content-Type": "application/json",
   },
