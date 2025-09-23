ca26cb77778e67a5bef3122c9f98eaa67be2994a
Yogesh <yogeshgadhewal28@gmail.com>
2025-09-23 12:09:13 +0530

    add the backend url

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -2,7 +2,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:4000/api", // base API url
+  // baseURL: "http://localhost:4000/api",
+    baseURL: "https://hrms-backend-jet.vercel.app/api",
   headers: {
     "Content-Type": "application/json",
   },
